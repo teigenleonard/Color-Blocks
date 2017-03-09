646de50597f25a48961f36c2a39ed98d8d241b93
Teigen Leonard <teigenl@gmail.com>
2017-03-09 17:34:27 -0600

    comments out if statement, adds individual listeners

--- Color Blocks/script.js
+++ Color Blocks/script.js
@@ -5,9 +5,36 @@ var yellowBlocks = 1;
 
 $(document).ready(function(){
   // console.log("hello!");
+  $(".container").on('click','#bluebutton', function(){
+    $(".container").append("<div class= 'blue'></div>");
+      numClicks += 1;
+  });
 
-  $(".container").append("<div class= 'blue'></div>");
-  $(".container").append("<div class= 'red'></div>");
-  $(".container").append("<div class= 'green'></div>");
-  $(".container").append("<div class= 'yellow'></div>");
+  $(".container").on('click','#redbutton', function(){
+    $(".container").append("<div class= 'red'></div>");
+      numClicks += 1;
+  });
+
+  $(".container").on('click','#greenbutton', function(){
+    $(".container").append("<div class= 'green'></div>");
+      numClicks += 1;
+  });
+
+  $(".container").on('click','#yellowbutton', function(){
+    $(".container").append("<div class= 'yellow'></div>");
+      numClicks += 1;
+  });
+  $(".container").on('click','button', function(){
+  //       if ('#bluebutton') {
+  //         blueBlocks += 1;
+  //       } else ('#redbutton') {
+  //         redBlocks += 1;
+  //       } else ('#greenbutton') {
+  //         greenBlocks += 1;
+  //       } else ('#yellowbutton') {
+  //         yellowBlocks += 1;
+  //       }
+  //
+  // })
+});
 });

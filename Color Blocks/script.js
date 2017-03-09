69cb1d9664994116f9a10d4cc7a1fcc850f02db5
Teigen Leonard <teigenl@gmail.com>
2017-03-09 16:17:54 -0600

    adds blocks and buttons

---
+++ Color Blocks/script.js
@@ -0,0 +1,13 @@
+var blueBlocks = 1;
+var redBlocks = 1;
+var greenBlocks = 1;
+var yellowBlocks = 1;
+
+$(document).ready(function(){
+  // console.log("hello!");
+
+  $(".container").append("<div class= 'blue'></div>");
+  $(".container").append("<div class= 'red'></div>");
+  $(".container").append("<div class= 'green'></div>");
+  $(".container").append("<div class= 'yellow'></div>");
+});

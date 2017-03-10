1d5f9cbe2bfa338273abb55817174ae97f66a595
Teigen Leonard <teigenl@gmail.com>
2017-03-09 23:34:49 -0600

    add counter to each color

--- Color Blocks/script.js
+++ Color Blocks/script.js
@@ -3,51 +3,47 @@ var redBlocks = 1;
 var greenBlocks = 1;
 var yellowBlocks = 1;
 
-$(document).ready(function(){
-  // console.log("hello!");
+$(document).ready(function() {
+    // console.log("hello!");
 
-  $(".blocks").append("<div class= 'blue'></div>");
-  $(".blocks").append("<div class= 'red'></div>");
-  $(".blocks").append("<div class= 'green'></div>");
-  $(".blocks").append("<div class= 'yellow'></div>");
-
-
-
-
-
-
-  $(".container").on('click','#bluebutton', function(){
     $(".blocks").append("<div class= 'blue'></div>");
-      // numClicks += 1;
-  });
+    $(".blocks").append("<div class= 'red'></div>");
+    $(".blocks").append("<div class= 'green'></div>");
+    $(".blocks").append("<div class= 'yellow'></div>");
 
-  $(".container").on('click','#redbutton', function(){
-  $(".blocks").append("<div class= 'red'></div>");
-      // numClicks += 1;
-  });
 
-  $(".container").on('click','#greenbutton', function(){
-  $(".blocks").append("<div class= 'green'></div>");
-      // numClicks += 1;
-  });
+    $(".container").on('click', '#bluebutton', function() {
+        $(".blocks").append("<div class= 'blue'></div>");
+        blueBlocks += 1;
+    });
 
-  $(".container").on('click','#yellowbutton', function(){
-  $(".blocks").append("<div class= 'yellow'></div>");
-      // numClicks += 1;
-  });
-  // $(".container").on('click','button', function(){
-  //       if ('#bluebutton') {
-  //         blueBlocks += 1;
-  //       } else ('#redbutton') {
-  //         redBlocks += 1;
-  //       } else ('#greenbutton') {
-  //         greenBlocks += 1;
-  //       } else ('#yellowbutton') {
-  //         yellowBlocks += 1;
-  //       }
-  //
-  // })
-// });
+    $(".container").on('click', '#redbutton', function() {
+        $(".blocks").append("<div class= 'red'></div>");
+        redBlocks += 1;
+    });
+
+    $(".container").on('click', '#greenbutton', function() {
+        $(".blocks").append("<div class= 'green'></div>");
+        greenBlocks += 1;
+    });
+
+    $(".container").on('click', '#yellowbutton', function() {
+        $(".blocks").append("<div class= 'yellow'></div>");
+        yellowBlocks += 1;
+    });
+    // $(".container").on('click','button', function(){
+    //       if ('#bluebutton') {
+    //         blueBlocks += 1;
+    //       } else ('#redbutton') {
+    //         redBlocks += 1;
+    //       } else ('#greenbutton') {
+    //         greenBlocks += 1;
+    //       } else ('#yellowbutton') {
+    //         yellowBlocks += 1;
+    //       }
+    //
+    // })
+    // });
 });
 
 // function addBlock() {

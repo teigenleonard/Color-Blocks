c2a31df3db6799d93e12df0d089454b9a482df4d
Teigen Leonard <teigenl@gmail.com>
2017-03-09 22:44:13 -0600

    add html class, organize code

--- Color Blocks/script.js
+++ Color Blocks/script.js
@@ -5,26 +5,34 @@ var yellowBlocks = 1;
 
 $(document).ready(function(){
   // console.log("hello!");
+
+  $(".container").append("<div class= 'blue'></div>");
+  $(".container").append("<div class= 'red'></div>");
+  $(".container").append("<div class= 'green'></div>");
+  $(".container").append("<div class= 'yellow'></div>");
+
+
+
+
+
+
   $(".container").on('click','#bluebutton', function(){
     $(".container").append("<div class= 'blue'></div>");
-      numClicks += 1;
+      // numClicks += 1;
   });
 
-  $(".container").on('click','#redbutton', function(){
-    $(".container").append("<div class= 'red'></div>");
-      numClicks += 1;
-  });
+  // $(".container").on('click','#redbutton', function(){
+      // numClicks += 1;
+  // });
 
-  $(".container").on('click','#greenbutton', function(){
-    $(".container").append("<div class= 'green'></div>");
-      numClicks += 1;
-  });
+  // $(".container").on('click','#greenbutton', function(){
+      // numClicks += 1;
+  // });
 
-  $(".container").on('click','#yellowbutton', function(){
-    $(".container").append("<div class= 'yellow'></div>");
-      numClicks += 1;
-  });
-  $(".container").on('click','button', function(){
+  // $(".container").on('click','#yellowbutton', function(){
+      // numClicks += 1;
+  // });
+  // $(".container").on('click','button', function(){
   //       if ('#bluebutton') {
   //         blueBlocks += 1;
   //       } else ('#redbutton') {
@@ -36,5 +44,5 @@ $(document).ready(function(){
   //       }
   //
   // })
-});
+// });
 });
